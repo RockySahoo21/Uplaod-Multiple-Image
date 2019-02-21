b0ce400732d22f73a515913e67bac2b4ee01e4ab
RockySahoo21 <40120448+RockySahoo21@users.noreply.github.com>
2019-02-21 17:04:23 +0530

    Update UdiChild.js

--- UdiChild.js
+++ UdiChild.js
@@ -1,40 +1,4 @@
-import React, { Component } from 'react';
-
-import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  Picker,
-  Image,
-  Button,
-  ScrollView
-} from 'react-native';
-
-import QRCodeScanner from 'react-native-qrcode-scanner';
-import { Dropdown } from 'react-native-material-dropdown';
-//import ImagePicker from 'react-native-image-picker';
 import ImagePicker from 'react-native-image-crop-picker';
-
-const options = {
-  title: 'Select Photo',
-  quality: 1.0,
-  width: 50,
-  height: 50,
-};
-
-
-export default class UdiChild extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { title: '',status:'',assetid:'',images:null};
-  }
-
-  dashboard = () => {
-    this.props.navigation.navigate('First');
-  }
 
   cameraOpen = () => {
     ImagePicker.openPicker({
@@ -64,47 +28,7 @@ export default class UdiChild extends Component {
     return (
       <View style={styles.container}>
 
-      <Text style={styles.centerText}>
-        Zed<Text style={styles.textBold}>Cloud</Text>
-      </Text>
-
-      <View style={{flex:1,flexDirection:'column',marginTop:50,marginBottom:50,justifyContent:'space-between',margin:10}}>
-
-      <View style={{flexDirection:'row'}}>
-        <Text style={{fontSize:20,fontWeight:'bold',color:'black',left:25}}>Title</Text>
-        <TextInput
-          style={{height: 40, borderColor: 'black', borderWidth: 1,width:150,left:100}}
-          onChangeText={(title) => this.setState({title})}
-          value={this.state.title}
-        />
-      </View>
-
-        <View style={{flexDirection:'row'}}>
-          <Text style={{fontSize:20,fontWeight:'bold',color:'black',left:25}}>Name</Text>
-          <TextInput
-            style={{height: 40, borderColor: 'black', borderWidth: 1,width:150,left:88}}
-            onChangeText={(status) => this.setState({status})}
-            value={this.state.status}
-          />
-        </View>
-
-        <View style={{flexDirection:'row'}}>
-          <Text style={{fontSize:20,fontWeight:'bold',color:'black',left:25}}>Asset ID</Text>
-          <TextInput
-            style={{height: 40, borderColor: 'black', borderWidth: 1,width:150,left:66}}
-            onChangeText={(assetid) => this.setState({assetid})}
-            value={this.state.assetid}
-          />
-        </View>
-
-        <View style={{justifyContent:'center',alignItems:'center'}}>
-
-        <TouchableOpacity style={{backgroundColor:'#fff',height: 40,width:150, borderColor: 'black', borderWidth: 1,alignItems:'center',justifyContent:'center'}} onPress={this.cameraOpen}>
-            <Text style={{fontWeight:'bold',fontSize:15,color:'black'}}>Add Site Image</Text>
-        </TouchableOpacity>
-
-          </View>
-
+     
         <View style={{flexDirection:'row',height:70,width:300,margin:5,justifyContent:'center',alignItems:'center'}}>
         <ScrollView
         horizontal={true}
@@ -116,40 +40,8 @@ export default class UdiChild extends Component {
           </ScrollView>
           </View>
 
-          <View style={{flexDirection:'row',marginTop:30,justifyContent:'center',alignItems:'center'}}>
-              <TouchableOpacity style={{backgroundColor:'#fff',height: 40,width:60, borderColor: 'black', borderWidth: 1,alignItems:'center',justifyContent:'center',right:60}} onPress={this.dashboard}>
-                 <Text style={{fontWeight:'bold',fontSize:15,color:'black'}}>Close</Text>
-             </TouchableOpacity>
-
-              <TouchableOpacity style={{backgroundColor:'#fff',height: 40,width:60, borderColor: 'black', borderWidth: 1,alignItems:'center',justifyContent:'center',left:60}} onPress={this.dashboard}>
-                  <Text style={{fontWeight:'bold',fontSize:15,color:'black'}}>Save</Text>
-              </TouchableOpacity>
-          </View>
-
-
-        </View>
-
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  centerText: {
-    fontSize: 25,
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  textBold: {
-    color: 'rgb(105, 179, 244)',
-  },
-  uploadAvatar: {
-  height:50,
-  width:50,
-  },
-});
